e6fd0929b7640d6198df6c42cd030f91d6dd498e
ivanhuay <ivan.huayrana@despegar.com>
2016-03-21 15:02:37 -0300

    cambio en la logica de movimiento generico en el servidor

--- js/main.js
+++ js/main.js
@@ -1,67 +1,52 @@
-var socket = io('http://10.254.48.21:3000');
-
-var caja = {
-    top: 50,
-    left: 50
-};
-var miId;
-socket.on('createBox', function(data) {
-	$(".caja").remove();
-
-    miId = data.id; //encodeURIComponent(id);
-    $(".row").append("<div class='caja' id='caja_" + miId + "'></div>")
-    console.log("#caja_" + miId);
-    $("#caja_" + miId).css({
-        "top": caja.top + "px",
-        "left": caja.left + "px",
-        "background-color":data.color
-    });
+(function($, io) {
+    var socket = io('http://10.254.48.24:3000');
+    var miId;
 
     $(document).on("keydown", function(e) {
         console.log(e.keyCode);
         switch (e.keyCode) {
             case 40:
-                socket.emit("up", caja);
+                socket.emit("up");
                 break;
             case 38:
-                socket.emit("down", caja);
+                socket.emit("down");
                 break;
             case 39:
-                socket.emit("right", caja);
+                socket.emit("right");
                 break;
             case 37:
-                socket.emit("left", caja);
+                socket.emit("left");
                 break;
         }
     });
-});
 
-socket.on("createPeople",function(data){
-	if($("#caja_" + data.id).length){
-		return 0;
-	}
-	$(".row").append("<div class='caja' id='caja_" + data.id + "'></div>")
-    console.log("#caja_" + data.id);
-    $("#caja_" + data.id).css({
-        "top": "50px",
-        "left": "50px",
-        "background-color":data.color
+    socket.on("createPeople", function(data) {
+        console.log("createPeople -->" + JSON.stringify(data));
+        if ($("#caja_" + data.id).length) {
+            console.log("Ya existe");
+            return 0;
+        }
+        $(".row").append("<div class='caja' id='caja_" + data.id + "'></div>")
+        console.log("#caja_" + data.id);
+        $("#caja_" + data.id).css({
+            "top": data.position.top + "px",
+            "left": data.position.left + "px",
+            "background-color": data.color
+        });
     });
-});
 
-socket.on("move", function(data) {
-    var position = data.position
-    var id = data.id;
+    socket.on("move", function(data) {
+        var position = data.position
+        var id = data.id;
 
-    if (id == miId) {
-        caja = position
-    }
-    $("#caja_" + id).css({
-        "top": position.top + "px",
-        "left": position.left + "px"
+        $("#caja_" + id).css({
+            "top": position.top + "px",
+            "left": position.left + "px"
+        });
     });
-});
 
-socket.on("deletePeople",function(id){
-	$("#caja_"+id).remove();
-});
+    socket.on("deletePeople", function(id) {
+        $("#caja_" + id).remove();
+    });
+
+})(jQuery, io);
